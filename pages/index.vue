fc708bac01da86a32533690b9967e93da13de0a7
bastorz <damianalcalayy@gmail.com>
2023-05-29 13:25:59 +0100

    header created

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,60 @@
 <template>
   <StoryblokComponent v-if="story" :blok="story.content" />
+  <div
+    class="flex items-center justify-between md:justify-evenly h-[94px] md:h-[99px] w-full bg-black space-x-20 md:space-x-4 gradient__bg"
+  >
+    <div class="hidden md:flex">
+      <img
+        src="Logo.png"
+        class="object-contain w-[97px] h-[50px]"
+        alt="The Rose Studio Logo"
+      />
+    </div>
+    <div class="flex md:hidden w-full items-center justify-between">
+      <img
+        src="Logo.png"
+        class="object-contain w-[97px] h-[50px] cursor-pointer"
+        alt="The Rose Studio Logo"
+      />
+      <DropdownMenu />
+    </div>
+    <div class="space-x-10 hidden md:flex">
+      <a href="/" class="headerLink">Portfolio</a>
+      <a href="/" class="headerLink">Servicios</a>
+      <a href="/" class="headerLink">Testimonios</a>
+      <a href="/" class="headerLink">Conversemos</a>
+      <a href="/" class="headerLink">Somos The Rose</a>
+    </div>
+    <div class="flex space-x-4">
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex cursor-pointer"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex cursor-pointer"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex cursor-pointer"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex cursor-pointer"
+      />
+    </div>
+  </div>
 </template>
 <script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 defineProps({ blok: Object });
 const story = await useAsyncStoryblok("home", {
   version: "draft",
--- pages/layout/header.vue
+++ pages/layout/header.vue
@@ -1,13 +1,73 @@
-<script setup lang="ts">
-definePageMeta({
-  // disable a default layout
-  layout: false,
-});
-const story = await useAsyncStoryblok("layout/header", {
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+  <div
+    class="flex items-center justify-evenly h-[94px] md:h-[99px] w-full bg-black space-x-4 gradient__bg"
+  >
+    <div class="flex">
+      <img
+        src="Logo.png"
+        class="object-contain w-[97px] h-[50px]"
+        alt="The Rose Studio Logo"
+      />
+      <!-- <fontAwesomeIcon
+          :icon="['fas', 'bars']"
+          style="color: #ffffff"
+          size="xl"
+          class="flex md:hidden"
+        /> -->
+    </div>
+    <div class="space-x-10 hidden md:flex">
+      <a class="headerLink">Portfolio</a>
+      <a class="headerLink">Servicios</a>
+      <a class="headerLink">Testimonios</a>
+      <a class="headerLink">Conversemos</a>
+      <a class="headerLink">Somos The Rose</a>
+    </div>
+    <div class="flex space-x-4">
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex"
+      />
+      <fontAwesomeIcon
+        :icon="['fas', 'bars']"
+        style="color: #ffffff"
+        size="xl"
+        class="hidden md:flex"
+      />
+    </div>
+  </div>
+  <!-- <div className="flex items-center space-x-4 text-sm font-light">
+      <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+      <p className="hidden lg:inline">Kids</p>
+      <BellIcon className="h-6 w-6" />
+      <Link href="/account">
+        <img
+          src="https://rb.gy/g1pwyx"
+          alt=""
+          className="cursor-pointer rounded"
+        />
+      </Link>
+    </div> -->
+</template>
+<script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+defineProps({ blok: Object });
+const story = await useAsyncStoryblok("home", {
   version: "draft",
 });
 </script>
-
-<template>
-  <StoryblokComponent v-if="story" :blok="story.content" />
-</template>
