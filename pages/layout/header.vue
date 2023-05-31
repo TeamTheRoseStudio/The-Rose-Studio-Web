e065095394c9b5b012ffed9ba0167d5d9d701042
alexey1254 <alxkill54@gmail.com>
2023-05-31 16:26:09 +0100

    Disabled layout, so it doesnt duplicates on storyblok

--- pages/layout/header.vue
+++ pages/layout/header.vue
@@ -3,6 +3,9 @@
 </template>
 <script setup>
 defineProps({ blok: Object });
+definePageMeta({
+  layout: false, // Hace que en Storyblok no se duplique el header / footer
+});
 const story = await useAsyncStoryblok("layout/header", {
   version: "draft",
 });
