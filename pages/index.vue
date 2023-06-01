a1e90b096c3d1f5aa5f8db1ac14e0fe5eaaf7eb8
bastorz <damianalcalayy@gmail.com>
2023-06-01 15:16:01 +0100

    fixed hero section

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,7 @@
   <!--STICKY BANNER-->
   <VisualImageWithHeading2></VisualImageWithHeading2>
   <FeatureListCards />
-
 </template>
-<script setup>
-
-</script>
+<script setup></script>
 <script setup></script>
 <style scoped></style>
