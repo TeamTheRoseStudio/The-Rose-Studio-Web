0e7b8997c2001df9edaedf4bd47990dde08eb11e
bastorz <damianalcalayy@gmail.com>
2023-06-04 19:02:34 +0100

    footer

--- storyblok/layout/mainFooter.vue
+++ storyblok/layout/mainFooter.vue
@@ -3,17 +3,17 @@
     <div
       class="grid grid-cols-1 place-items-center place-content-center py-20 gap-y-6 gradient_bg_orange_footer px-4"
     >
-      <img src="/Logo.png" alt="The Rose Studio Logo" />
+      <img src="/Logo.png" alt="The Rose Studio Logo" class="mb-4" />
       <p class="text-white/50">
         Open-source library of over 400+ web components and interactive elements
         built for better web
       </p>
-      <div class="grid grid-cols-3 place-content-center place-items-center">
-        <p class="text-white/50">Somos The Rose</p>
-        <p class="text-white/50 w-full text-center">Política de privacidad</p>
-        <p class="text-white/50">Configuración de cookies</p>
+      <div class="grid grid-cols-3 place-items-center place-content-center">
+        <p class="text-white">Somos The Rose</p>
+        <p class="text-white w-full text-center">Política de privacidad</p>
+        <p class="text-white">Configuración de cookies</p>
       </div>
-      <p class="text-white/80 text-lg">
+      <p class="text-white/50 text-lg">
         © 2023 The Rose Studio. Todos los derechos reservados.
       </p>
     </div>
