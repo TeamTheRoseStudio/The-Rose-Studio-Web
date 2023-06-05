31bdca52b4521628d9745dbbed85961ad7598981
alexey1254 <alxkill54@gmail.com>
2023-06-05 07:32:05 +0100

    Finished defaultFeatureList

--- storyblok/organisms/marketing/featureSection/defaultFeatureList.vue
+++ storyblok/organisms/marketing/featureSection/defaultFeatureList.vue
@@ -1,20 +1,60 @@
 <template>
-  <section class="bg-white dark:bg-gray-900" v-editable="blok">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-      <div class="max-w-screen-md mb-8 lg:mb-16">
-        <div
-          class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
-        >
-          {{ blok.title }}
+  <section
+    class="bg-dark gradient_bg_orange_services mb-20 h-full"
+    v-editable="blok"
+  >
+    <div class="gradient_bg_purple_services">
+      <div
+        class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 gradient_bg_orange_services_mobile"
+      >
+        <div class="md:grid md:grid-cols-3 md:w-full text-center mb-8 lg:mb-16">
+          <div>
+            <div
+              class="mb-4 text-start text-5xl tracking-tight font-extrabold text-white"
+              v-html="renderRichText(blok.title)"
+            ></div>
+            <div
+              class="bg-gradient-to-r from-[#8A2387] to-[#F26021] h-[2.81px] w-[190px] mb-3"
+            ></div>
+          </div>
+          <div
+            class="md:col-span-2 md:ms-10 text-start font-light text-white dark:text-gray-400 sm:text-xl"
+            v-html="renderRichText(blok.paragraph)"
+          ></div>
         </div>
-        <p class="text-gray-500 sm:text-xl dark:text-gray-400">
-          {{ blok.paragraph }}
-        </p>
+        <!--Services GRID -->
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 pl-2 pt-20 gap-y-20 md:gap-x-4 lg:gap-x-10"
+        >
+          <div class="space-y-4" v-for="item in blok.items" v-editable="item">
+            <NuxtImg
+              provider="storyblok"
+              format="webp"
+              :preload="true"
+              :alt="item.image.alt"
+              :src="item.image.filename"
+            />
+            <div
+              class="text-white text-3xl"
+              v-html="renderRichText(item.title)"
+            ></div>
+            <div
+              class="text-white"
+              v-html="renderRichText(item.paragraph)"
+            ></div>
+            <div
+              class="md:ml-0 bg-gradient-to-r from-[#8A2387] to-[#F26021] pr-5 inline-flex items-center justify-center"
+            >
+              <StoryblokComponent
+                v-for="blok_item in item.button"
+                :key="blok_item._uid"
+                :blok="blok_item"
+                class="text-white"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <StoryblokComponent
-      v-for='blok_item in blok.grid'
-      :key='blok_item._uid'
-      :blok='blok_item' />
     </div>
   </section>
 </template>
