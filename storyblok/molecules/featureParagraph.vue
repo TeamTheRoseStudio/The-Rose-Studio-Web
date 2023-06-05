a1ac9381e56a17f8a78c463090917a9b97ae1df8
alexey1254 <alxkill54@gmail.com>
2023-06-05 09:59:22 +0100

    Added slug

---
+++ storyblok/molecules/featureParagraph.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-editable="blok" class=" bg-white">
+    <div class="ms-4 sm:text-center">
+      <div class="prose sm:mx-auto pt-10" v-html="renderRichText(blok.title)"></div>
+      <div
+        class="mb-10 sm:mx-auto bg-gradient-to-r from-[#8A2387] to-[#F26021] h-[2.81px] w-[190px]"
+      ></div>
+    </div>
+
+    <div class="prose mx-4 " v-html="renderRichText(blok.paragraph)"></div>
+  </div>
+</template>
+<script setup>
+defineProps({ blok: Object });
+</script>
+<style scoped></style>
