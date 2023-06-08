19dbd223ebf0145bf2b0b922778542af1d61b9b8
alexey1254 <alxkill54@gmail.com>
2023-06-08 09:47:53 +0100

    changed font

--- storyblok/organisms/marketing/serviceSections/cardHowWeDevelopYourIdea.vue
+++ storyblok/organisms/marketing/serviceSections/cardHowWeDevelopYourIdea.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-editable="blok" class="space-y-4">
-    <h3
+    <div
       v-html="renderRichText(blok.title)"
       class="text-5xl bg-clip-text text-transparent bg-gradient-to-r from-[#8A2387] to-[#F26021]"
-    ></h3>
+    ></div>
     <div
       v-html="renderRichText(blok.title2)"
       class="text-2xl font-semibold"
