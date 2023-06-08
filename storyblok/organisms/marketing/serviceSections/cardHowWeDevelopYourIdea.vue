94cb8ca4eeb4e607835a1fbd1bd2657a02aa25b3
alexey1254 <alxkill54@gmail.com>
2023-06-08 09:45:26 +0100

    hotfixed bug

--- storyblok/organisms/marketing/serviceSections/cardHowWeDevelopYourIdea.vue
+++ storyblok/organisms/marketing/serviceSections/cardHowWeDevelopYourIdea.vue
@@ -4,14 +4,14 @@
       v-html="renderRichText(blok.title)"
       class="text-5xl bg-clip-text text-transparent bg-gradient-to-r from-[#8A2387] to-[#F26021]"
     ></h3>
-    <h4
+    <div
       v-html="renderRichText(blok.title2)"
       class="text-2xl font-semibold"
-    ></h4>
-    <p
+    ></div>
+    <div
       v-html="renderRichText(blok.paragraph)"
       class="text-lg text-gray-400"
-    ></p>
+    ></div>
   </div>
 </template>
 <script setup>
