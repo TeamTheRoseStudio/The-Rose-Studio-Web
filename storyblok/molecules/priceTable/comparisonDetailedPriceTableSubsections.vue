76725e6e0e9ed904b189fd0c0e442ada1cddf28f
alexey1254 <alxkill54@gmail.com>
2023-06-13 12:45:26 +0100

    Fixed

--- storyblok/molecules/priceTable/comparisonDetailedPriceTableSubsections.vue
+++ storyblok/molecules/priceTable/comparisonDetailedPriceTableSubsections.vue
@@ -5,9 +5,7 @@
     <!-- detailed plan comparison items -->
     <div v-html="renderRichText(blok.title)" class="text-white"></div>
     <!-- detailed plan comparison check icons -->
-    <div v-if="blok.listItemPriceIcon.icon">
-      <FontAwesomeIcon :icon="blok.listItemPriceIcon.icon" /> 
-    </div>
+    <FontAwesomeIcon :icon="blok.ListItemPriceIcon.icon" />
   </div>
 </template>
 <script setup>
