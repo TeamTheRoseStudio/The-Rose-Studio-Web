a814412728f6bbba00958c2d8e42db67e90345d7
damian <damianalcalayy@gmail.com>
2023-06-15 11:38:59 +0100

    cree el componente alert para atoms

--- storyblok/molecules/priceTable/toggleExtraPriceTable.vue
+++ storyblok/molecules/priceTable/toggleExtraPriceTable.vue
@@ -1,9 +1,14 @@
 <template>
-  <StoryblokComponent
-      v-for="blok_item in blok.ButtonToggleDetailedComparisonPriceTable"
-      :key="blok_item._uid"
-      :blok="blok_item"
-    />
+<div>
+      <button
+      v-html="renderRichText(blok.title)"
+      type="button"
+      class="flex justify-center items-center mx-auto font-medium text-white dark-text-primary-400 hover:text-primary-700"
+      data-collapse-toggle="detailed-pricing"
+    >
+  
+    </button>
+</div>
   <div id="detailed-pricing" class="hidden overflow-x-auto mt-8 w-full">
     <StoryblokComponent
       v-for="blok_item in blok.ComparisonDetailedPriceTable"
